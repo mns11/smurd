5ba5549aff28072076b2d776c15dafd84283c23e
mns11 <dstselin@gmail.com>
2022-09-29 19:33:26 +0300

    SMURD-1: added Spring Boot skeleton to the project.

---
+++ src/main/java/com/github/smurd/SmurdApplication.java
@@ -0,0 +1,13 @@
+package com.github.smurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/smurd/SmurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
